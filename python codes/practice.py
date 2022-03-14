45c419a50e8c204787c875ee6af03edbce7d6dfb
gunavenkat <54988447+gunavenkat@users.noreply.github.com>
2022-03-14 21:10:55 +0530

    updation of files

--- python codes/practice.py
+++ python codes/practice.py
@@ -1,6 +1,6 @@
 import face_recognition
-known_image = face_recognition.load_image_file("media/face.jpg")
-unknown_image = face_recognition.load_image_file("media/face1.jpg")
+known_image = face_recognition.load_image_file("media/profiles/face.jpg")
+unknown_image = face_recognition.load_image_file("media/user_uploaded/WIN_20220314_11_04_16_Pro.jpg")
 
 biden_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
